9d09fef58966e9783e1e86f69377869607a15e14
4ev3r - Vetiti Studios Co Founder <j.r.burks09@gmail.com>
2026-05-27 15:50:00 -0500

    test

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD6oFRc1zChwlmDEQQTixwBbo6cJ-A9e9M",
+  authDomain: "videosite-426c1.firebaseapp.com",
+  projectId: "videosite-426c1",
+  storageBucket: "videosite-426c1.firebasestorage.app",
+  messagingSenderId: "112937784499",
+  appId: "1:112937784499:web:24e6bd8e1549f0eadd8cc6"
+};
